b9443ed8184e418206c9100e3ab0caf715e1b326
Richard <richarddzh@163.com>
2024-09-18 09:19:39 +0000

    Single thruster works now

--- Lander.cpp
+++ Lander.cpp
@@ -170,7 +170,10 @@ const int LT_FAIL_ONLY = 1;
 const int RT_FAIL_ONLY = 2;
 const int LT_OK_ONLY = 3;
 const int RT_OK_ONLY = 4;
+const int MT_OK_ONLY = 5;
 static int Control_Fail_Mode = OK_ALL;
+static double BaseAngle = 0;
+static bool ShouldRotate = true;
 const int SensorSampleCount = 100;
 const int ErrorSampleCount = 20;
 const double PosNoiseThreshold = 40;
@@ -179,7 +182,9 @@ const double X_MIN = 0;
 const double X_MAX = 1024;
 const double Y_MIN = 0;
 const double Y_MAX = 1024;
+const double MAX_VELOCITY = 12;
 typedef double (*SampleFunc)();
+typedef void (*ThrusterFunc)(double power);
 double GetMeanAndError(SampleFunc sampelFunc, double *stdErr, const char *name);
 double Position_X_robust(void);
 double Position_Y_robust(void);
@@ -187,15 +192,58 @@ double Velocity_X_robust(void);
 double Velocity_Y_robust(void);
 double Angle_robust(void);
 bool NearLanding(double pos_x, double pos_y);
-bool Rotate_robust(double angle, double pos_x, double pos_y);
-void Thruster_robust(double targetVx, double targetVy, double vx, double vy, const char *name);
+void SingleThrusterControl(double delVx, double delVy, double angle);
+bool Rotate_robust(double angle, double pos_x, double pos_y, double vx, double vy, double targetVx, double targetVy);
+void Thruster_robust(double targetVx, double targetVy, double vx, double vy, double angle, const char *name);
 double fmin_Dist(double x, double y);
+double fargmin_VectorDiff(double fx, double fy, double targetX, double targetY);
+void fargmin_VectorDiff2(double& power1, double& power2, double fx1, double fy1, double fx2, double fy2, double targetX, double targetY);
 
 double fmin_Dist(double x, double y)
 {
  if (x < 0) return y;
  if (y < 0) return x;
  return fmin(x, y);
+}
+
+double fargmin_VectorDiff(double fx, double fy, double targetX, double targetY)
+{
+ // Minimumize Diff = (power*fx-targetX)^2 + (power*fy-targetY)^2
+ // Subject to power in [0, 1] range
+ // Diff = power^2*(fx^2+fy^2) - 2*power*(fx*targetX+fy*targetY) + targetX^2+targetY^2
+ double a = fx*fx + fy*fy;
+ double b = -2 * (fx*targetX + fy*targetY);
+ // double c = targetX*targetX + targetY*targetY;
+ // double power = -b / 2 / a;
+ double power = (fx*targetX + fy*targetY)/(fx*fx + fy*fy);
+ if (power < 0) power = 0;
+ if (power > 1) power = 1;
+ return power;
+}
+
+void fargmin_VectorDiff2(double& power1, double& power2, double fx1, double fy1, double fx2, double fy2, double targetX, double targetY)
+{
+ // Minimumize Diff = (power1*fx1+power2*fx2-targetX)^2 + (power1*fy1+power2*fy2-targetY)^2
+ // Subject to power1, power2 in [0, 1] range
+ // Let power1*fx1+power2*fx2 = targetX, power1*fy1+power2*fy2 = targetY
+ power1 = (targetX*fy2 - targetY*fx2) / (fx1*fy2 - fx2*fy1);
+ power2 = (targetY*fx1 - targetX*fy1) / (fx1*fy2 - fx2*fy1);
+ if (power1 >= 0 && power1 <= 1 && power2 >= 0 && power2 <= 1) {
+  return;
+ }
+ // If the solution is out of the range, we should find the closest point on the line.
+ if (power1 < 0 || power1 > 1) {
+  if (power1 < 0) power1 = 0;
+  if (power1 > 1) power1 = 1;
+  power2 = fargmin_VectorDiff(fx2, fy2, targetX - power1*fx1, targetY - power1*fy1);
+  return;
+ }
+ if (power2 < 0 || power2 > 1) {
+  if (power2 < 0) power2 = 0;
+  if (power2 > 1) power2 = 1;
+  power1 = fargmin_VectorDiff(fx1, fy1, targetX - power2*fx2, targetY - power2*fy2);
+  return;
+ }
 }
 
 double GetMeanAndError(SampleFunc sampelFunc, double *stdErr, const char *name)
@@ -276,12 +324,16 @@ double Angle_robust(void)
 bool NearLanding(double pos_x, double pos_y)
 {
  printf("PLAT_X=%.2f,PLAT_Y=%.2f,pos_x=%.2f,pos_y=%.2f\n", PLAT_X, PLAT_Y, pos_x, pos_y);
- return fabs(PLAT_X - pos_x) < 50 && fabs(PLAT_Y - pos_y) < 50;
+ return fabs(PLAT_X - pos_x) < 30 && fabs(PLAT_Y - pos_y) < 30;
 }
 
-bool Rotate_robust(double angle, double pos_x, double pos_y)
+bool Rotate_robust(double angle, double pos_x, double pos_y, double vx, double vy, double targetVx, double targetVy)
 {
+ // When prepare landing, the lander should rotate to the direction of the landing platform.
  bool prepareLanding = NearLanding(pos_x, pos_y);
+ 
+ // If all thrusters are OK, or the lander is near the landing platform
+ // The lander should keep the angle of 0 degree.
  if ((MT_OK && RT_OK && LT_OK) || prepareLanding)
  {
   Control_Fail_Mode = OK_ALL;
@@ -293,7 +345,14 @@ bool Rotate_robust(double angle, double pos_x, double pos_y)
   }
   return false;
  }
- else if (MT_OK && !LT_OK && RT_OK)
+ 
+ // If only the left thruster is failed, the lander should rotate right by 45 degrees.
+ // So that the main thruster pushes the lander to the up-right direction.
+ // And the right thruster pushes the lander to the up-left direction.
+ //           Lander
+ //         /        \
+ //       MT          RT
+ if (MT_OK && !LT_OK && RT_OK)
  {
   Control_Fail_Mode = LT_FAIL_ONLY;
   if (angle < 44 || (angle > 46 && angle <= 225)) Rotate(45-angle);
@@ -301,7 +360,14 @@ bool Rotate_robust(double angle, double pos_x, double pos_y)
   else return false;
   return true;
  }
- else if (MT_OK && LT_OK && !RT_OK)
+ 
+ // If only the right thruster is failed, the lander should rotate left by 45 degrees.
+ // So that the main thruster pushes the lander to the up-left direction.
+ // And the left thruster pushes the lander to the up-right direction.
+ //           Lander
+ //         /        \
+ //       LT          MT
+ if (MT_OK && LT_OK && !RT_OK)
  {
   Control_Fail_Mode = RT_FAIL_ONLY;
   if (angle > 316 || (angle > 135 && angle < 314)) Rotate(315-angle);
@@ -309,64 +375,169 @@ bool Rotate_robust(double angle, double pos_x, double pos_y)
   else return false;
   return true;
  }
+
+ // Otherwise, if any thruster fails
+ // The lander should use the OK thruster to push the lander to the up direction.
+ if (LT_OK)
+ {
+  Control_Fail_Mode = LT_OK_ONLY;
+  BaseAngle = 270;
+ }
+ else if (RT_OK)
+ {
+  Control_Fail_Mode = RT_OK_ONLY;
+  BaseAngle = 90;
+ }
+ else
+ {
+  Control_Fail_Mode = MT_OK_ONLY;
+  BaseAngle = 0;
+ }
+
+ printf("Rotate_robust: angle=%.2f,BaseAngle=%.2f\n", angle, BaseAngle);
+ // No rotation is needed.
  return false;
 }
 
-void Thruster_robust(double targetVx, double targetVy, double vx, double vy, const char *name)
+void SingleThrusterControl(double delVx, double delVy, double angle)
 {
- double delVx = targetVx - vx;
- double delVy = targetVy - vy;
- // Calculate the acceleration vector.
- // By default, make it in the direction upwards from -45 to 45 degrees.
+ if (delVx == 0 && delVy == 0) return;
+
+ // When there is only one thruster OK
+ // The lander should rotate and adjust thruster power in the meantime.
+ // We iteratively repeat between rotation and thrust adjustment.
+ if (ShouldRotate)
+ {
+  const double maxGoalAngle = 45;
+  const double maxRotateAngle = 2;
+  const double minRotateAngle = 1;
+  double goalAngle = 90 - atan2(delVy, delVx) / M_PI * 180;
+  if (goalAngle > maxGoalAngle) goalAngle = maxGoalAngle;
+  if (goalAngle < -maxGoalAngle) goalAngle = -maxGoalAngle;
+  double targetAngle = BaseAngle + goalAngle;
+  if (targetAngle < 0) targetAngle += 360;
+  if (targetAngle >= 360) targetAngle -= 360;
+  double angleDiff = targetAngle - angle;
+  if (angleDiff > 180) angleDiff -= 360;
+  if (angleDiff < -180) angleDiff += 360;
+  if (angleDiff < -maxRotateAngle) angleDiff = -maxRotateAngle;
+  if (angleDiff > maxRotateAngle) angleDiff = maxRotateAngle;
+  if (fabs(angleDiff) > minRotateAngle)
+  {
+   printf("SingleThrusterControl: angle=%.2f,goalAngle=%.2f,targetAngle=%.2f,angleDiff=%.2f\n",
+    angle, goalAngle, targetAngle, angleDiff);
+   Rotate(angleDiff);
+  }
+   ShouldRotate = false;
+   return;
+ }
+ // Adjust the thruster power.
+ double powerAngle = angle - BaseAngle;
+ double fx = sin(powerAngle * M_PI / 180);
+ double fy = cos(powerAngle * M_PI / 180);
+ ThrusterFunc Thruster = NULL;
  switch (Control_Fail_Mode)
  {
-  default:
+  case LT_OK_ONLY:
   {
-   double absV = sqrt(delVx*delVx + delVy*delVy);
-   if (absV >= G_ACCEL * sqrt(0.5)) {
-    double ratio = G_ACCEL * sqrt(0.5) / absV;
-    delVx *= ratio;
-    delVy *= ratio;
-   }
-   delVy += G_ACCEL;
+   fx *= LT_ACCEL;
+   fy *= LT_ACCEL;
+   Thruster = Left_Thruster;
+   Main_Thruster(0);
+   Right_Thruster(0);
+   break;
+  }
+  case RT_OK_ONLY:
+  {
+   fx *= RT_ACCEL;
+   fy *= RT_ACCEL;
+   Thruster = Right_Thruster;
+   Main_Thruster(0);
+   Left_Thruster(0);
+   break;
+  }
+  case MT_OK_ONLY:
+  {
+   fx *= MT_ACCEL;
+   fy *= MT_ACCEL;
+   Thruster = Main_Thruster;
+   Left_Thruster(0);
+   Right_Thruster(0);
    break;
   }
  }
+
+ double power = fargmin_VectorDiff(fx, fy, delVx, delVy);
+ printf("SingleThrusterControl: delVx=%.2f,delVy=%.2f,fx=%.2f,fy=%.2f,power=%.2f,powerAngle=%.2f\n",
+  delVx, delVy, fx, fy, power, powerAngle);
+ Thruster(power);
+
+ if (!ShouldRotate)
+ {
+  ShouldRotate = true;
+ }
+}
+
+void Thruster_robust(double targetVx, double targetVy, double vx, double vy, double angle, const char *name)
+{
+ double delVx = targetVx - vx;
+ double delVy = targetVy - vy + G_ACCEL;
+ // Limit speed to maximum velocity.
+ double absVel = sqrt(vx*vx + vy*vy);
+ if (absVel > MAX_VELOCITY)
+ {
+  double maxVx = vx / absVel * MAX_VELOCITY;
+  double maxVy = vy / absVel * MAX_VELOCITY;
+  delVx += maxVx - vx;
+  delVy += maxVy - vy;
+ }
+
  printf("%s: delVx=%.2f,delVy=%.2f,targetVx-vx=%.2f,targetVy-vy=%.2f\n",
   name, delVx, delVy, targetVx-vx, targetVy-vy);
  double powerMain = 0;
  double powerLeft = 0;
  double powerRight = 0;
- double accel = 1;
  switch (Control_Fail_Mode)
  {
+  case LT_OK_ONLY:
+  case RT_OK_ONLY:
+  case MT_OK_ONLY:
+  {
+   SingleThrusterControl(delVx, delVy, angle);
+   return;
+  }
   case LT_FAIL_ONLY:
   {
-   powerMain = fmax(delVx+delVy, 0)/sqrt(2)/MT_ACCEL*accel;
-   powerRight = fmax(delVy-delVx, 0)/sqrt(2)/RT_ACCEL*accel;
+   powerLeft = 0;
+   fargmin_VectorDiff2(powerMain, powerRight,
+    MT_ACCEL * sqrt(0.5), MT_ACCEL * sqrt(0.5),
+    -RT_ACCEL * sqrt(0.5), RT_ACCEL * sqrt(0.5),
+    delVx, delVy);
    break;
   }
   case RT_FAIL_ONLY:
   {
-   powerMain = fmax(delVy-delVx, 0)/sqrt(2)/MT_ACCEL*accel;
-   powerLeft = fmax(delVx+delVy, 0)/sqrt(2)/LT_ACCEL*accel;
+   powerRight = 0;
+   fargmin_VectorDiff2(powerMain, powerLeft,
+    -MT_ACCEL * sqrt(0.5), MT_ACCEL * sqrt(0.5),
+    LT_ACCEL * sqrt(0.5), LT_ACCEL * sqrt(0.5),
+    delVx, delVy);
    break;
   }
   default:
   {
-   powerMain = fmax(delVy, 0)/MT_ACCEL*accel;
-   if (delVx > 0) powerLeft = fmax(delVx, 0)/LT_ACCEL*accel;
-   else powerRight = fmax(-delVx, 0)/RT_ACCEL*accel;
+   powerRight = 0;
+   fargmin_VectorDiff2(powerMain, powerLeft,
+    0, MT_ACCEL, LT_ACCEL, 0,
+    delVx, delVy);
+   if (powerLeft < 0) {
+    powerRight = -powerLeft;
+    powerLeft = 0;
+   }
    break;
   }
  }
- double normalizeRatio = fmax(powerMain/MT_ACCEL,
-  fmax(powerLeft/LT_ACCEL, powerRight/RT_ACCEL));
- if (normalizeRatio > 1) {
-  powerMain /= normalizeRatio;
-  powerLeft /= normalizeRatio;
-  powerRight /= normalizeRatio;
- }
+
  printf("%s: powerMain=%.2f,powerLeft=%.2f,powerRight=%.2f\n",
   name, powerMain, powerLeft, powerRight);
  Main_Thruster(powerMain);
@@ -442,50 +613,31 @@ void Lander_Control(void)
  double vel_x = Velocity_X_robust();
  double vel_y = Velocity_Y_robust();
  double angle = Angle_robust();
+
+ // Set the velocity limits.
  double distX = fabs(PLAT_X - pos_x);
  double distY = fabs(PLAT_Y - pos_y);
  if (distX > 200) {
-  VXlim = 30;
+  VXlim = MAX_VELOCITY;
   VYlim = 0;
  } else if (distX > 50) {
-  VXlim = 15;
+  VXlim = MAX_VELOCITY / 2;
  } else if (distX < 5) {
   VXlim = 0;
  } else {
-  VXlim = 5;
+  VXlim = MAX_VELOCITY / 4;
  }
  if (distX < 200) {
-  if (distY > 200) {
-   VYlim = -20;
-  } else if (distY > 40) {
-   VYlim = -10;
+  if (distY > 400) {
+   VYlim = -MAX_VELOCITY;
+  } else if (distY > 200) {
+   VYlim = -MAX_VELOCITY / 2;
   } else {
-   VYlim = -4;
+   VYlim = -MAX_VELOCITY / 4;
   }
  }
 
- printf("Robust: X=%.2f,Y=%.2f,Vx=%.2f,Vy=%.2f,Angle=%.2f,VXlim=%.2f,VYlim=%.2f\n",
-  pos_x, pos_y, vel_x, vel_y, angle, VXlim, VYlim);
-
- // IMPORTANT NOTE: The code below assumes all components working
- // properly. IT MAY OR MAY NOT BE USEFUL TO YOU when components
- // fail. More likely, you will need a set of case-based code
- // chunks, each of which works under particular failure conditions.
-
- // Check for rotation away from zero degrees - Rotate first,
- // use thrusters only when not rotating to avoid adding
- // velocity components along the rotation directions
- // Note that only the latest Rotate() command has any
- // effect, i.e. the rotation angle does not accumulate
- // for successive calls.
-
- if (Rotate_robust(angle, pos_x, pos_y))
- {
-  return;
- }
-
- // Module is oriented properly, check for horizontal position
- // and set thrusters appropriately.
+ // Set the target horizontal velocity to the correct direction.
  double targetVx = 0;
  if (pos_x>PLAT_X)
  {
@@ -496,7 +648,25 @@ void Lander_Control(void)
   targetVx = VXlim;
  }
 
- Thruster_robust(targetVx, VYlim, vel_x, vel_y, "Lander_Control");
+ printf("Robust: X=%.2f,Y=%.2f,Vx=%.2f,Vy=%.2f,Angle=%.2f,VXlim=%.2f,VYlim=%.2f\n",
+  pos_x, pos_y, vel_x, vel_y, angle, VXlim, VYlim);
+
+ // IMPORTANT NOTE: The code below assumes all components working
+ // properly. IT MAY OR MAY NOT BE USEFUL TO YOU when components
+ // fail. More likely, you will need a set of case-based code
+ // chunks, each of which works under particular failure conditions.
+
+ // Rotate the lander to the correct direction.
+ if (Rotate_robust(angle, pos_x, pos_y, vel_x, vel_y, targetVx, VYlim))
+ {
+  return;
+ }
+
+ // Adjust thrusters to move the lander to the correct direction.
+ // Force of the thrusters should be adjusted according to acceleration.
+ // The acceleration equals to the change in the velocity.
+ // That is, the target velocity vector minus the current velocity vector.
+ Thruster_robust(targetVx, VYlim, vel_x, vel_y, angle, "Lander_Control");
 }
 
 void Safety_Override(void)
@@ -563,18 +733,27 @@ void Safety_Override(void)
  {
   double alpha = i * M_PI / 18;
   double dist = SONAR_DIST[i];
+  // Since sonar waves are not reflected by the map boundary
+  // We explicitly mimic the boundary by setting the distance to the boundary.
   if (i < 3 || i > 33) dist = fmin_Dist(dist, (pos_y-Y_MIN)/cos(alpha));
   if (i > 6 && i < 12) dist = fmin_Dist(dist, (X_MAX-pos_x)/sin(alpha));
   if (i > 15 && i < 21) dist = fmin_Dist(dist, (Y_MAX-pos_y)/cos(alpha-M_PI));
   if (i > 24 && i < 30) dist = fmin_Dist(dist, (pos_x-X_MIN)/sin(alpha-M_PI));
+  // If the distance is far away, ignore it.
   if (dist < 0 || dist > DistLimit) continue;
+  // Simulate the force that pushes the lander away from the obstacle.
+  // The force is inversely proportional to the square of the distance.
+  // This is like the force between two charges with the same sign.
   sumX -= sin(alpha)/fmax(1, dist)/fmax(1,dist)*DistLimit*DistLimit;
   sumY -= cos(alpha)/fmax(1, dist)/fmax(1,dist)*DistLimit*DistLimit;
+  // If the lander has no obstacle in the horizontal direction
+  // We should push it to the direction of the landing platform.
   if ((i == 9 || i == 27) && (dist < 0 || dist > DistLimit)) {
-    sumX += (PLAT_X - pos_x) / 50;
+    sumX += (PLAT_X - pos_x) / 100;
   }
  }
  
+ // Keep the lander away from the top boundary.
  if (pos_y < fmin(DistLimit/2,60))
  {
   sumY = fmin(0, sumY);
@@ -593,6 +772,6 @@ void Safety_Override(void)
 
  if (targetVx != 0 || targetVy != 0)
  {
-  Thruster_robust(targetVx, targetVy, vel_x, vel_y, "Safety_Override");
+  Thruster_robust(targetVx, targetVy, vel_x, vel_y, angle, "Safety_Override");
  }
 }
